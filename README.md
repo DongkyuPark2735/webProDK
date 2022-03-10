6bf34676a21ca542269e7032a4f7b4ebe3bf601b
DongkyuPark2735 <qer2735@gmail.com>
2022-03-10 16:41:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+웹 프로그래밍을 익히기 위한 Java-DBMS-WebStandard_JavaScript 
+공유폴더 입니다. by DK
---
+++ source/01_java/ch01/src/Hello.java
@@ -0,0 +1,11 @@
+/*
+ 2022.3.10 DK 
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+		System.out.print("Welcome~");
+		System.out.print("³¡");
+		
+		}
+}
